baee9d70a214e7f909b2570a8e9a4a6c34ea6bfb
lynnjiang <lynnjinjie@gmail.com>
2024-09-15 19:44:45 +0800

    feat(dashboard): add categories chart

--- components/spending-pie.tsx
+++ components/spending-pie.tsx
@@ -1,11 +1,5 @@
 import { useState } from 'react'
-import {
-  AreaChart,
-  BarChart,
-  FileSearch,
-  LineChart,
-  Loader2,
-} from 'lucide-react'
+import { FileSearch, Loader2, PieChart, Radar, Target } from 'lucide-react'
 import {
   Select,
   SelectContent,
@@ -14,21 +8,20 @@ import {
   SelectValue,
 } from '@/components/ui/select'
 import { Card, CardContent, CardTitle, CardHeader } from '@/components/ui/card'
-import { AreaVariant } from '@/components/area-variant'
-import { BarVariant } from '@/components/bar-variant'
-import { LineVariant } from '@/components/line-variant'
-import { Skeleton } from './ui/skeleton'
+import { PieVariant } from '@/components/pie-variant'
+import { RadarVariant } from '@/components/radar-variant'
+import { RadialVariant } from '@/components/radial-variant'
+import { Skeleton } from '@/components/ui/skeleton'
 
 type Props = {
   data?: {
-    date: string
-    income: number
-    expenses: number
+    name: string
+    value: number
   }[]
 }
 
-export const Chart = ({ data = [] }: Props) => {
-  const [chartType, setChartType] = useState('area')
+export const SpendingPie = ({ data = [] }: Props) => {
+  const [chartType, setChartType] = useState('pie')
 
   const onTypeChange = (type: string) => {
     // TODO: add paywall
@@ -38,28 +31,28 @@ export const Chart = ({ data = [] }: Props) => {
   return (
     <Card className="border-none drop-shadow-sm">
       <CardHeader className="flex space-y-2 lg:space-y-0 lg:flex-row lg:items-center justify-between">
-        <CardTitle className="text-xl line-clamp-1">Transactions</CardTitle>
+        <CardTitle className="text-xl line-clamp-1">Categories</CardTitle>
         <Select defaultValue={chartType} onValueChange={onTypeChange}>
           <SelectTrigger className="lg:w-auto h-9 rounded-md px-3">
             <SelectValue placeholder="Select a chart" />
           </SelectTrigger>
           <SelectContent>
-            <SelectItem value="area">
+            <SelectItem value="pie">
               <div className="flex items-center">
-                <AreaChart className="size-4 mr-2 shrink-0" />
-                <p className="line-clamp-1">Area Chart</p>
+                <PieChart className="size-4 mr-2 shrink-0" />
+                <p className="line-clamp-1">Pie Chart</p>
               </div>
             </SelectItem>
-            <SelectItem value="bar">
+            <SelectItem value="radar">
               <div className="flex items-center">
-                <BarChart className="size-4 mr-2 shrink-0" />
-                <p className="line-clamp-1">Bar Chart</p>
+                <Radar className="size-4 mr-2 shrink-0" />
+                <p className="line-clamp-1">Radar Chart</p>
               </div>
             </SelectItem>
-            <SelectItem value="line">
+            <SelectItem value="radial">
               <div className="flex items-center">
-                <LineChart className="size-4 mr-2 shrink-0" />
-                <p className="line-clamp-1">Line Chart</p>
+                <Target className="size-4 mr-2 shrink-0" />
+                <p className="line-clamp-1">Radial Chart</p>
               </div>
             </SelectItem>
           </SelectContent>
@@ -75,17 +68,16 @@ export const Chart = ({ data = [] }: Props) => {
           </div>
         ) : (
           <>
-            {chartType === 'area' && <AreaVariant data={data} />}
-            {chartType === 'bar' && <BarVariant data={data} />}
-            {chartType === 'line' && <LineVariant data={data} />}
+            {chartType === 'pie' && <PieVariant data={data} />}
+            {chartType === 'radar' && <RadarVariant data={data} />}
+            {chartType === 'radial' && <RadialVariant data={data} />}
           </>
         )}
       </CardContent>
     </Card>
   )
 }
-
-export const ChartLoading = () => {
+export const SpendingPieLoading = () => {
   return (
     <Card className="border-none drop-shadow-sm">
       <CardHeader className="flex space-y-2 lg:space-y-0 lg:flex-row lg:items-center justify-between">
